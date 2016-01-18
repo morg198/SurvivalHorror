7e1964797929ece8fddcff651af9e7e355263836
morg198 <morg198@yahoo.com>
2016-01-18 15:57:17 -0800

    Initial Sync

---
+++ Assets/Scripts/followAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followAI : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
